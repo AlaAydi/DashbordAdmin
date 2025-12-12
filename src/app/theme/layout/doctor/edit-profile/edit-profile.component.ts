48072bfbe98b5934908a3ee9580e2fc1fd6e8ef3
AlaAydi <contact.aydiala@gmail.com>
2025-12-12 22:00:46 +0100

    adding component edit profile

--- src/app/theme/layout/doctor/edit-profile/edit-profile.component.ts
+++ src/app/theme/layout/doctor/edit-profile/edit-profile.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-profile',
-  imports: [],
+  standalone :true ,
+    imports: [ReactiveFormsModule],
+
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.scss'
 })
 export class EditProfileComponent {
 
+  profileForm = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl(''),
+    address: new FormControl(''),
+    speciality: new FormControl('Médecin Généraliste', Validators.required),
+    experience: new FormControl(1),
+    workingHours: new FormControl('08h00 - 17h00'),
+    internalId: new FormControl('DOC-001')
+  });
+   saveProfile() {
+    if (this.profileForm.valid) {
+      console.log("Profil médecin enregistré :", this.profileForm.value);
+    }
+  }
 }
